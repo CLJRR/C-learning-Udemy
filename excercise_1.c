a5fb8a4a04e0c3cd51d131817759cbdb5ab82f68
CLJRR <calvinleejiarong@gmail.com>
2022-12-11 20:38:34 +0800

    first commit

---
+++ excercise_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+  printf("we have %d coins in the bank\n",100);
+  printf("we have %.1f coins in the bank\n", 125.7);
+  printf("Year = %d\n",2020);
+  printf("your average grade is %.2f\n",95.13);
+  printf("num 1 = %d num 2 = %d sum= %d\n",5,7,5+7);
+  printf("num 1 = %.1f num 2 = %.1f sum = %.1f\n", 5.2,7.3,5.2+7.3);
+  printf("num 1 = %d num 2 = %d sub = %d\n",5,3,5-3);
+  printf("a = %d b = %d sum = a + b = %d\n",3,5,3+5);
+  printf("a = %d b = %d sum = %d + %d = %d\n", 3,5,3,5,3+5);
+  printf("a = %d b = %d sum = a + b = %d + %d = %d\n", 3,5,3,5,3+5);
+}
