c5b57563804c860cb61a237a3580918b4a3de305
CLJRR <calvinleejiarong@gmail.com>
2022-12-24 12:29:43 +0800

    sorted

--- tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-  timeMinutes = remainingDistance / (speed/60.0); // 60 / (80/60) -->
-  remainingDistance = remainingDistance/(speed/60.0);
-  printf("remaining dist S %d\n",remainingDistance);
-  timeSeconds = remainingDistance/(speed/360.0);
-  printf("The time it will take is %d hours,%f minutes,%2d seconds",timeHours,timeMinutes,timeSeconds);
-  return 0;
