5539a03076f2cd86e28a53570fae5c08b1857bdd
CLJRR <calvinleejiarong@gmail.com>
2022-12-25 23:52:44 +0800

    firstcommit

---
+++ reference/collation2.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int selection, x, y; // integer %d is integers
+  float b;             // floating numbers  %f is for floating numbers
+  double c, x;         // large floatring numbers
+  printf("1 - casting example\n"
+         "2 - if else example\n"
+         "3 - While loop example\n"
+         "please enter selection:\n");
+  scanf("%d", &selection);
+  sleep(1); // wait 1 seconds
+  switch (selection)
+  {
+  case '1':
+    casting;
+    break;
+  case '2':
+    ifelse;
+
+  default:
+    break;
+  }
+}
+void casting()
+{
+  int x, y;
+  x = 4;
+  y = 3;
+  double result, castedResult;
+  result = x / y;
+  castedResult = x / (double)y;
+  printf("The uncasted result is %.2lf\nThe casted result is %.2lf\n\n", result, castedResult);
+  result = x / 3;
+  castedResult = x / (double)(1 + 2);
+  printf("The uncasted result is %.2lf\nThe casted result is %.2lf\n", result, castedResult);
+}
+void ifelse()
+{
+  int x, y;
+  printf("please enter value of x:  ");
+  scanf("%d", &x);
+  printf("please enter value of y:  ");
+  scanf("%d", &y);
+  if (x > y)
+  {
+    printf("x is bigger than y\n");
+  }
+  else if (x < y)
+  {
+    printf("y is bigger than x\n");
+  }
+  else if (x == y)
+  {
+    printf("x is equal to y\n");
+  }
+  else
+  {
+    printf("you have entered an invalid selection, please re-enter x and y");
+  }
+}
