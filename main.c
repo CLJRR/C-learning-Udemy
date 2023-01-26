86eeb89be159ab77a59a86d6076c40f6a2cdbdc9
CLJRR <calvinleejiarong@gmail.com>
2023-01-26 19:12:53 +0800

    f commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int divisor(int input)
+{
+  if
+}
+int main()
+{
+  int input;
+  printf("please enter number");
+  scanf("%d", &input);
+  divisor(input);
+}
