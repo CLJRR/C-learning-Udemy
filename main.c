c17bb2d963b4bf4410bd077bd3daa20f61c3dc0b
CLJRR <calvinleejiarong@gmail.com>
2023-01-29 16:41:44 +0800

    29 jan

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int sumOfDivisors()
+{
+}
+
+int main()
+{
+  int input;
+  printf("please insert a number");
+  scanf("%d", &input);
+  sumOfDivisors(input);
+}
