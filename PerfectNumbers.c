e86e4dc9665b60a5f4c511ff8949a0433025eb4c
CLJRR <calvinleejiarong@gmail.com>
2023-02-02 15:18:35 +0800

    2/2

--- PerfectNumbers.c
+++ PerfectNumbers.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+int perfectNumber(int num)
+{
+  int result = 1, i;
+  if (num == 1)
+    return 1;
+  for (i = 2; ii++)
+}
 int main()
 {
+  int input;
+  printf("please enter a number");
+  scanf("%d", &input);
+
+  if (perfectNumber(input) == 1)
+    printf("perfect number");
+  else
+    printf("Not perfect number");
 }
