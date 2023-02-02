17bd2e0e45ef53bcdcade4bf31a8a9e8691acdcd
CLJRR <calvinleejiarong@gmail.com>
2023-02-02 23:20:14 +0800

    2 feb

--- PerfectNumbers.c
+++ PerfectNumbers.c
@@ -5,15 +5,23 @@ int perfectNumber(int num)
   int result = 1, i;
   if (num == 1)
     return 1;
-  for (i = 2; ii++)
+  for (i = 2; i * i < num; i++)
+
+    if (num % i == 0)
+      result += i + (num / i);
+
+  if (i * i == num)
+    result += i;
+
+  return result;
 }
 int main()
 {
   int input;
-  printf("please enter a number");
+  printf("please enter a number: ");
   scanf("%d", &input);
 
-  if (perfectNumber(input) == 1)
+  if (perfectNumber(input) == input)
     printf("perfect number");
   else
     printf("Not perfect number");
