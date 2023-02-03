085b63e743a0a44ffcd88f47f9c592dd15ceafab
CLJRR <calvinleejiarong@gmail.com>
2023-02-03 22:24:42 +0800

    2/3

--- ArrayWeather.c
+++ ArrayWeather.c
@@ -5,14 +5,12 @@
 int main()
 {
   int i;
-  double temperature[size], averageTemp, hottest, coldest;
+  double temperature[size], averageTemp = 0, hottest = 0, coldest = 0;
   for (i = 0; i < size; i++)
   {
     printf("please enter temp %d: ", i + 1);
     scanf("%lf", &temperature[i]);
   }
-  hottest = temperature[0];
-  coldest = temperature[0];
   printf("cold: %lf\nhot: %lf\n", coldest, hottest);
   for (i = 0; i < size; i++)
   {
