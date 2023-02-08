ab3d14fbeb5c2ed49c5b684aad415ce961e2ff36
CLJRR <calvinleejiarong@gmail.com>
2023-02-09 00:15:55 +0800

    9 feb

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 2
 int main()
 {
+  int dateArrayDay[SIZE];
+  int dateArrayMonth[SIZE];
+  int dateArrayYear[SIZE];
+  int inputArray[3];
+  int i, j;
+  for (i = 0; i < SIZE; i++)
+  {
+    printf("Please enter date %d in [DD MM YYYY] format: ", i + 1);
+    scanf("%d %d %d", &inputArray[0], &inputArray[1], &inputArray[2]);
+
+    dateArrayDay[i] = inputArray[0];
+    dateArrayMonth[i] = inputArray[1];
+    dateArrayYear[i] = inputArray[2];
+  }
 }
