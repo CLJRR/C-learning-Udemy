ea37ad99ca8b2310f82c1031c3219244d9e7fb32
CLJRR <calvinleejiarong@gmail.com>
2023-02-09 22:15:52 +0800

    9 feb

--- main.c
+++ main.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 2
+#define SIZE 7
 int main()
 {
-  int dateArrayDay[SIZE];
-  int dateArrayMonth[SIZE];
-  int dateArrayYear[SIZE];
-  int inputArray[3];
-  int i, j;
-  for (i = 0; i < SIZE; i++)
+  int i;
+  int array[SIZE] = {5, 3, 4, 1, 4, 3, 5};
+  for (i = 0; i < SIZE / 2; i++)
   {
-    printf("Please enter date %d in [DD MM YYYY] format: ", i + 1);
-    scanf("%d %d %d", &inputArray[0], &inputArray[1], &inputArray[2]);
-
-    dateArrayDay[i] = inputArray[0];
-    dateArrayMonth[i] = inputArray[1];
-    dateArrayYear[i] = inputArray[2];
+    if (array[i] != array[SIZE - 1 - i])
+    {
+      printf("not palindrome");
+      return 0;
+    }
   }
+  printf("palindrome");
 }
