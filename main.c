7c7306ba3c1f382be5d56395ea76bb5ca7f41416
CLJR <calvinleejiarong@gmail.com>
2023-08-14 11:03:28 +0800

    test

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(void)
   for (int i = 0; i < n; i++)
   {
     printf("please enter name %d: ", i + 1);
-    scanf("%s", names[i]);
+  scanf("%s", names[i]);
   }
   for (int i = 0; i < n; i++)
   {
